e4539ac6ef20abff6741f76035671fc5e6d6f1e5
nidameltem95@gmail.com <nidameltem95@gmail.com>
2021-02-03 02:19:52 +0300

    Add project files.

---
+++ Entities/Abstract/IEntity.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Entities.Abstract
+{
+    //IEntity implement eden bir 
+    public interface IEntity
+    {
+
+
+
+    }
+}
